5f10ee03ceb2b5276dc4ef16b5dead4aa943335b
Saumya171 <40817398+Saumya171@users.noreply.github.com>
2021-07-26 22:02:36 +0530

    Update automation.sh with cron job

--- automation.sh
+++ automation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-s3_bucket=upgrad-saumya
+s3_bucket="upgrad-saumya"
+file="/var/www/html/inventory.html"
 sudo apt update -y
 sudo apt-get install apache2
 if [ $(/etc/init.d/apache2 status | grep -v grep | grep 'Apache2 is running' | wc -l) > 0 ]
@@ -16,3 +17,12 @@ sudo apt install awscli
 aws s3 \
 cp /tmp/saumya-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar \
 s3://$(s3_bucket)/saumya-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar
+
+if [[ -f "$file" ]];
+then
+    echo " found."
+else
+    echo "not found."
+fi
+
+00 08-16 * * * /root/Automation_Project/automation.sh
